3cbf99d0b43cc473c96f7995fa1127e103d444f9
1244026470 <1244026470@qq.com>
2019-06-06 11:22:52 +0800

    234234

--- 1.js
+++ 1.js
@@ -1,9 +1,1 @@
-var arr=[1,3,2,4,5,9,6,5,3,2,4,9]
-
-
-var pivotIndex = Math.floor(arr.length / 2);
-
-// 　　var pivot = arr.splice(pivotIndex, 1)[0];
-console.log(pivotIndex)
-// console.log(pivot)
-console.log(arr.splice(pivotIndex, 1)[0])
+console.log((0.07).toFixed(0)+'%')
