5956581388c00f3256d92058732884e2e90b2f97
1244026470 <1244026470@qq.com>
2019-08-05 17:54:37 +0800

    2342

---
+++ 1.js
@@ -0,0 +1,53 @@
+// function stringToByte(str) {
+//   var bytes = new Array();
+//   var len, c;
+//   len = str.length;
+//   for (var i = 0; i < len; i++) {
+//       c = str.charCodeAt(i);
+//       if (c >= 0x010000 && c <= 0x10FFFF) {
+//           bytes.push(((c >> 18) & 0x07) | 0xF0);
+//           bytes.push(((c >> 12) & 0x3F) | 0x80);
+//           bytes.push(((c >> 6) & 0x3F) | 0x80);
+//           bytes.push((c & 0x3F) | 0x80);
+//       } else if (c >= 0x000800 && c <= 0x00FFFF) {
+//           bytes.push(((c >> 12) & 0x0F) | 0xE0);
+//           bytes.push(((c >> 6) & 0x3F) | 0x80);
+//           bytes.push((c & 0x3F) | 0x80);
+//       } else if (c >= 0x000080 && c <= 0x0007FF) {
+//           bytes.push(((c >> 6) & 0x1F) | 0xC0);
+//           bytes.push((c & 0x3F) | 0x80);
+//       } else {
+//           bytes.push(c & 0xFF);
+//       }
+//   }
+//   return bytes;
+
+// }
+
+const obj={
+  MobileNumber: "13733443259",
+  CodeType: 1
+}
+const requestStr=JSON.stringify(obj)
+console.log(requestStr)
+
+// const byte=stringToByte(requestStr)
+// // console.log(byte)
+
+// const byte2=byte.map((item)=>{
+//   return ~item;
+// })
+
+// console.log(byte2)
+
+// console.log(btoa("byte2"))
+
+
+function strToUint8(str){
+  for (var i = 0,arr=[]; i < str.length;i++){
+    arr.push(str.charCodeAt(i));
+  }
+  return arr;
+}
+
+console.log(strToUint8(requestStr))
